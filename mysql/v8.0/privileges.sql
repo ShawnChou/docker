38a43e350a8f9f447898bd8527b40539c0c44153
Shawn Chou <shawnchou1988@yeah.net>
2020-06-22 19:50:05 +0800

    fix bug

--- mysql/v8.0/privileges.sql
+++ mysql/v8.0/privileges.sql
@@ -1,4 +1,4 @@
 use mysql;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '123456' PASSWORD EXPIRE NEVER;
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
 flush privileges;
