79e584b8e975a7b702e41aeb3af697df8e0ccaab
Shawn Chou <shawnchou1988@yeah.net>
2020-06-22 17:29:05 +0800

    fix bug

--- mysql/v8.0/setup.sh
+++ mysql/v8.0/setup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
-#查看mysql服务的状态，方便调试，这条语句可以删除
-echo `service mysql status`
-echo '1.启动mysql....'
 #启动mysql
-service mysql start
+mysqld
 sleep 3
-echo `service mysql status`
-echo '2.还原mysql_native_password验证'
+#还原mysql_native_password验证
 mysql < /mysql/privileges.sql
 tail -f /dev/null
