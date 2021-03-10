9f70952ac07b3627ec3fbe3ea86bd50a6491458a
Shawn <shawnchou1988@yeah.net>
2021-03-10 20:42:42 +0800

    Update privileges.sql

--- mysql/v8.0/privileges.sql
+++ mysql/v8.0/privileges.sql
@@ -2,3 +2,4 @@ use mysql;
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456';
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
 flush privileges;
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
